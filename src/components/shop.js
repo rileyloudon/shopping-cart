73f7553907842a31b4734a8c41e8b1e07bb0a78b
Riley Loudon <rileyloudon@icloud.com>
2021-05-30 21:34:26 -0500

    change header to h2

--- src/components/shop.js
+++ src/components/shop.js
@@ -4,7 +4,7 @@ import ShopItem from './shop-item';
 const Shop = ({ shopItems }) => {
   return (
     <div className='shop'>
-      <h1>We hope you find what you need!</h1>
+      <h2>We hope you find what you need!</h2>
       <div className='shop-items'>
         {shopItems.map((item) => (
           <ShopItem key={item.name} item={item} />
