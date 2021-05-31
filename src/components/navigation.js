8b2600df215d4144d947d526249cf9fea8574195
Riley Loudon <rileyloudon@icloud.com>
2021-05-30 21:54:09 -0500

    change tags from header to nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,14 +3,14 @@ import cartImg from '../img/shopping_cart_24dp.svg';
 
 const Navigation = ({ cart }) => {
   return (
-    <header className='navigation'>
+    <nav>
       <Link to='/'>Home</Link>
       <Link to='/shop'>Shop</Link>
       <Link to='/checkout'>
-        <p id='cart-number'>{cart.length}</p>
+        <p>{cart.length}</p>
         <img src={cartImg} alt='Shopping Cart' />
       </Link>
-    </header>
+    </nav>
   );
 };
 
